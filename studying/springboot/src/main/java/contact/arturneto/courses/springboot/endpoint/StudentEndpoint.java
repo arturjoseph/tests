a34870a8484ddc91a83749caa1c3e8ed78929b13
Artur Neto <arturjoseph@gmail.com>
2019-08-23 17:02:56 -0300

    Seurity antMachers

--- studying/springboot/src/main/java/contact/arturneto/courses/springboot/endpoint/StudentEndpoint.java
+++ studying/springboot/src/main/java/contact/arturneto/courses/springboot/endpoint/StudentEndpoint.java
@@ -6,14 +6,13 @@ import contact.arturneto.courses.springboot.repository.StudentRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@RequestMapping("/students")
+@RequestMapping("v1")
 @RestController
 public class StudentEndpoint {
 
@@ -23,36 +22,35 @@ public class StudentEndpoint {
         this.studentDAO = studentDAO;
     }
 
-    @GetMapping
+    @GetMapping("protected/students")
     public ResponseEntity<Page<Student>> getAll(Pageable pageable) {
         return ResponseEntity.ok().body(studentDAO.findAll(pageable));
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("protected/students/{id}")
     public ResponseEntity<Student> getById(@PathVariable("id") Long id) {
         return ResponseEntity.ok().body(studentDAO.findById(id).orElseThrow(() -> new ResourceNotFoundException("Student not found for ID: " + id)));
     }
 
-    @GetMapping("findByName/{name}")
+    @GetMapping("protected/students/findByName/{name}")
     public ResponseEntity<List<Student>> getByname(@PathVariable("name") String name) {
         return ResponseEntity.ok().body(studentDAO.findByNameIgnoreCaseContaining(name));
     }
 
-    @PostMapping
+    @PostMapping("admin/students")
     public ResponseEntity<Student> save(@Valid @RequestBody Student student) {
         studentDAO.save(student);
         return ResponseEntity.created(URI.create("http://localhost:8080/students/" + student.getId())).body(student);
     }
 
-    @PutMapping
+    @PutMapping("admin/students")
     public ResponseEntity<Student> update(@RequestBody Student student) {
         verifyStudentExists(student.getId());
         studentDAO.save(student);
         return ResponseEntity.accepted().build();
     }
 
-    @DeleteMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("admin/students/{id}")
     public ResponseEntity<Student> delete(@PathVariable("id") Long id) {
         verifyStudentExists(id);
         studentDAO.deleteById(id);

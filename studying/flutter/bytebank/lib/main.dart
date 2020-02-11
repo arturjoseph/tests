0d112de85edff40e47f995b61fd5e5c1928e95b3
Artur Neto <arturjoseph@gmail.com>
2020-02-11 16:56:37 -0300

    bytebanck with persistence

--- studying/flutter/bytebank/lib/main.dart
+++ studying/flutter/bytebank/lib/main.dart
@@ -1,21 +1,25 @@
-import 'package:bytebank/screens/tranferencia/lista.dart';
+import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(ByteBankApp());
+void main() {
+  runApp(Bytebank());
+}
 
-class ByteBankApp extends StatelessWidget {
+class Bytebank extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bytebank',
       theme: ThemeData(
-        primaryColor: Colors.green[900],
-        accentColor: Colors.blueAccent[700],
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blueAccent[700],
-          textTheme: ButtonTextTheme.primary
-        )
-      ),
-      home:  ListaTransferencias(),
+          primaryColor: Colors.green[900],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
+          )),
+      home: Dashboard()
     );
   }
 }
+

83664ae9dc51038849e6efb98f934db84461d15d
Artur Neto <arturjoseph@gmail.com>
2020-03-04 23:08:29 -0300

    Aprendendo C#

---
+++ studying/AprendendoCSharp/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo, primeiro programa com visual studio!");
+            Console.ReadLine();
+        }
+    }
+}

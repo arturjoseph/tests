0bb6b2975ee6c19510fd2f393b5090db5986090f
Artur Neto <arturjoseph@gmail.com>
2024-09-30 23:14:52 -0300

    finalizando curso de php seguro

---
+++ formacao-php/php-mvc-seguranca/tabela-videos.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+
+$pdo->exec('CREATE TABLE videos (id INTEGER PRIMARY KEY, url TEXT, filePath TEXT);');
